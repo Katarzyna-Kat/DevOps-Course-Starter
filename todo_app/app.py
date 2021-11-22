6f34ebcddb851cdf83e0cc7475e6f6fd84aac409
Katarzyna-Kat <k.polonkiewicz@gmail.com>
2021-11-22 15:05:48 +0000

    first commit

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+from todo_app.data.session_items import get_items
 
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config())
 
-
+#getting the list
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html', get_items = get_items)
