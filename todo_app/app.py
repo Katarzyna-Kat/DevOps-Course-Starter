cbc499e7bea95236020b52c0851fc6b5e3befe90
Katarzyna-Kat <k.polonkiewicz@gmail.com>
2021-11-23 16:45:56 +0000

    added button

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from todo_app.data.session_items import get_items
+from flask import Flask, render_template, request
+from todo_app.data.session_items import get_items, add_item
 
 from todo_app.flask_config import Config
 
@@ -7,6 +7,14 @@ app = Flask(__name__)
 app.config.from_object(Config())
 
 #getting the list
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html', get_items = get_items)
+
+
+#adding the list
+@app.route('/add', methods=["POST"])
+def add():
+    newitem = request.form['title']
+    add_item(newitem)
+    return render_template('index.html')
