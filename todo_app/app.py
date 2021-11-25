d659ebb06b49718f4e9e016c1e3df43c6ee954ea
Katarzyna-Kat <k.polonkiewicz@gmail.com>
2021-11-25 16:14:35 +0000

    works

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from todo_app.data.session_items import get_items, add_item
 
 from todo_app.flask_config import Config
@@ -6,16 +6,16 @@ from todo_app.flask_config import Config
 app = Flask(__name__)
 app.config.from_object(Config())
 
-#getting the list
-@app.route('/', methods=['GET'])
+# getting the list
+@app.route("/", methods=["GET"])
 def index():
-    return render_template('index.html', get_items = get_items)
+    return render_template("index.html", items=get_items())
 
 
-#adding to the list
-#this is not working - need to read more for it
-@app.route('/add', methods=["POST"])
+# adding to the list
+# this is not working
+@app.route("/add", methods=["POST"])
 def add():
-    newitem = request.form['title']
+    newitem = request.form["title"]
     add_item(newitem)
-    return render_template('index.html')
+    return redirect(url_for("index"))
