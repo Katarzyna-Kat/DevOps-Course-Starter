880ace7d5164c0db839428cc347b183c4224ea3f
-loper-kpolonkiewicz <-loper-kpolonkiewicz@uk.kworld.kpmg.com>
2022-06-17 10:52:49 +0100

    hate this

--- todo_app/app.py
+++ todo_app/app.py
@@ -17,66 +17,69 @@ from todo_app.class_items import Item
 from todo_app.view_model_class import ViewModel
 
 
-app = Flask(__name__)
-app.config.from_object(Config())
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config())
 
 
-# fetching
+    # fetching
 
 
-@app.route("/", methods=["GET"])
-def index():
-    all_items = show_cards()
-    view_model = ViewModel(all_items)
-    return render_template(
-        "index.html",
-        view_model=view_model,
-    )
+    @app.route("/", methods=["GET"])
+    def index():
+        all_items = show_cards()
+        view_model = ViewModel(all_items)
+        return render_template(
+            "index.html",
+            view_model=view_model,
+        )
 
 
-# create init function lines 27-39
+    # create init function lines 27-39
 
-# add item
-
-
-@app.route("/add", methods=["POST"])
-def add():
-    newitem = request.form["name"]
-    add_item(newitem)
-    return redirect(url_for("index"))
+    # add item
 
 
-# deleteing item from list
+    @app.route("/add", methods=["POST"])
+    def add():
+        newitem = request.form["name"]
+        add_item(newitem)
+        return redirect(url_for("index"))
 
 
-@app.route("/delete/<id>", methods=["POST"])
-def delete(id):
-    delete_item(id=request.form["Remove_id"])
-    return redirect(url_for("index"))
+    # deleteing item from list
 
 
-# moving to todo
+    @app.route("/delete/<id>", methods=["POST"])
+    def delete(id):
+        delete_item(id=request.form["Remove_id"])
+        return redirect(url_for("index"))
 
 
-@app.route("/into_todo/<id>", methods=["POST"])
-def move_todo(id):
-    move_to_todo(id=request.form["ToDo_id"])
-    return redirect(url_for("index"))
+    # moving to todo
 
 
-# moving to progress
+    @app.route("/into_todo/<id>", methods=["POST"])
+    def move_todo(id):
+        move_to_todo(id=request.form["ToDo_id"])
+        return redirect(url_for("index"))
 
 
-@app.route("/into_progress/<id>", methods=["POST"])
-def move_progress(id):
-    move_to_progress(id=request.form["Progress_id"])
-    return redirect(url_for("index"))
+    # moving to progress
 
 
-# moving to done
+    @app.route("/into_progress/<id>", methods=["POST"])
+    def move_progress(id):
+        move_to_progress(id=request.form["Progress_id"])
+        return redirect(url_for("index"))
 
 
-@app.route("/into_done/<id>", methods=["POST"])
-def move_done(id):
-    move_to_done(id=request.form["Done_id"])
-    return redirect(url_for("index"))
+    # moving to done
+
+
+    @app.route("/into_done/<id>", methods=["POST"])
+    def move_done(id):
+        move_to_done(id=request.form["Done_id"])
+        return redirect(url_for("index"))
+
+    return app
